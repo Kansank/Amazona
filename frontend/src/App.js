dafd2f71285c6925b39b236e53e577e31728c58f
Kansank <kansankinthana@gmail.com>
2020-10-15 15:29:46 +0700

    EP6 16.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import {BrowserRouter, Link, Route} from 'react-router-dom';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
-  const cart = useSelector(state => state.cart);
-  const {cartItems} = cart;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
-    <BrowserRouter> 
-    <div className="grid-container">
-  <header className="row">
-    <div>
-      <Link className="brand" to="/">amazona</Link>
-    </div>
-    <div className="header-links">
-      <Link to="/cart">Cart
-      {cartItems.length > 0 && (
-        <span className="badge"> {cartItems.length} </span>
-      )}
-      </Link>
-      <Link to="/signin">Sign In</Link>
-    </div>
-  </header>
-  <main className="main">
-    <Route path="/cart/:id?" component={CartScreen}></Route>
-    <Route path="/product/:id" component={ProductScreen}></Route>
-    <Route path="/" component={HomeScreen} exact></Route>
-    
-  </main>
-  <footer className="row center">
-    All right reserved.
-  </footer>
-</div>
-</BrowserRouter>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div className="header-links">
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge"> {cartItems.length} </span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
+          </div>
+        </header>
+        <main className="main">
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,16 +3,16 @@ import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 const initialState = {
-    cart:{
-        cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems'))
-        :[],
+    cart: {
+      cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
     },
-};
+  };
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
