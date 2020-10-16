f305a37a72bc74b7e29c6d5f1c3faaa9d5a8fe65
Kansank <kansankinthana@gmail.com>
2020-10-16 21:39:40 +0700

    EP8 23.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {applyMiddleware, createStore, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducers';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 const initialState = {
   userSignin: {
     userInfo: localStorage.getItem('userInfo')
@@ -20,6 +20,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
