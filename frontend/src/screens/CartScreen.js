48aa09c48cd1e89f1d309efcc4a0ec1ae575c2e8
Kansank <kansankinthana@gmail.com>
2020-10-16 16:16:08 +0700

    EP8 23.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { addToCart, removeFromCart } from "../actions/cartAction";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 import MessageBox from "../components/MessageBox";
 
 export default function CartScreen(props) {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,13 @@ import {applyMiddleware, createStore, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
     cart: {
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
@@ -13,6 +19,7 @@ const reducer = combineReducers({
     productList : productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
