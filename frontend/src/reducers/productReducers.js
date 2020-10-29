d113914f4b12a9353ad11d23d9835563b1609ce0
Kansank <kansankinthana@gmail.com>
2020-10-29 16:49:53 +0700

    No_41.

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_RESET, PRODUCT_CREATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET } = require("../constants/productConstants");
+const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_RESET, PRODUCT_CREATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_RESET } = require("../constants/productConstants");
 
 export const productListReducer = (
     state = { loading: true,products: [] }, action) => {
@@ -51,6 +51,21 @@ export const productDetailsReducer = (state = { loading: true }, action) => {
       case PRODUCT_UPDATE_FAIL:
         return { loading: false, error: action.payload };
       case PRODUCT_UPDATE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+
+  export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_DELETE_REQUEST:
+        return { loading: true };
+      case PRODUCT_DELETE_SUCCESS:
+        return { loading: false, success: true };
+      case PRODUCT_DELETE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_DELETE_RESET:
         return {};
       default:
         return state;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {applyMiddleware, createStore, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer } from './reducers/orderReducers';
-import { productCreateReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducers';
+import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducers';
 import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
 const initialState = {
   userSignin: {
@@ -34,6 +34,7 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productDelete: productDeleteReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
